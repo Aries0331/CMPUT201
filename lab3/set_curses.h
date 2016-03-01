d8a1d30f377eb7b8fdd369033adc4222ffce8f94
Aries0331 <jinzhu@ualberta.ca>
2016-03-01 12:22:49 -0700

    Added files via upload

---
+++ lab3/set_curses.h
@@ -0,0 +1,11 @@
+/*  ONE Card number: 1461696
+ Computing Science UNIX ID: jinzhu
+ lecture section: A2
+ lab section: D01
+ TA's name: Chenyang Huang  */
+
+#include <assert.h>
+#include <curses.h>
+
+void init_ncurse();
+void shutdown_ncurse();
